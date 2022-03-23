2092ef1c62c55d2f77b7267f95f243fe08ff34f3
clases clases <clases@labinfo.is.escuelaing.edu.co>
2022-03-23 15:04:11 -0500

    Secure Spark APP

---
+++ src/main/java/com/edu/eci/secureSparkApp/SparkMain.java
@@ -0,0 +1,28 @@
+package com.edu.eci.secureSparkApp;
+
+import static spark.Spark.*;
+
+public class SparkMain {
+    public static void main(String[] args) {
+        //API: secure(keystoreFilePath, keystorePassword, truststoreFilePath,truststorePassword);
+        secure(getKeyStore(), "123456", null, null);
+        //port(5000);
+        port(getPort());
+        get("/hello", (req, res) -> "Hello Heroku!!!");
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 5000; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+    static String getKeyStore() {
+        if (System.getenv("PORT") != null) {
+            return String.valueOf(Integer.parseInt(System.getenv("KEYSTORE")));
+        }
+        return "keystore/ecikeystore.p12"; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+
+}
